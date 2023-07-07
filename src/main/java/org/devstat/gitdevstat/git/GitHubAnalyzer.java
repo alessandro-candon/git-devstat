13fc37b36d4652dd672fd7f2b3a5449efd2d8d9d
ACANDO14 <alessandro.candon@decathlon.com>
2023-07-07 16:00:42 +0200

    feat(jgit): list commits

--- src/main/java/org/devstat/gitdevstat/git/GitHubAnalyzer.java
+++ src/main/java/org/devstat/gitdevstat/git/GitHubAnalyzer.java
@@ -4,12 +4,16 @@ package org.devstat.gitdevstat.git;
 import static org.devstat.gitdevstat.AppProperties.APP_NAME;
 
 import java.io.File;
+import java.io.IOException;
 import org.devstat.gitdevstat.AppProperties;
 import org.devstat.gitdevstat.client.gitprovider.dto.RepositoryDto;
 import org.devstat.gitdevstat.utils.FsUtil;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +23,12 @@ public class GitHubAnalyzer implements IGitAnalyzer {
     private final AppProperties appProperties;
     private final FsUtil fs;
 
+    private final String workdir;
+
     public GitHubAnalyzer(AppProperties appProperties, FsUtil fsUtil) {
         this.appProperties = appProperties;
         this.fs = fsUtil;
+        this.workdir = appProperties.tmpDir() + "/" + APP_NAME;
     }
 
     public void clone(RepositoryDto repositoryDto) {
@@ -38,9 +45,25 @@ public class GitHubAnalyzer implements IGitAnalyzer {
         }
     }
 
-    public void stat(RepositoryDto repositoryDto) {
+    public void stat(RepositoryDto repositoryDto) throws IOException, GitAPIException {
         if (!fs.repoFolderExists(repositoryDto)) {
             clone(repositoryDto);
         }
+        Repository repository = getExistentGitRepository(repositoryDto);
+        Git git = new Git(repository);
+        Iterable<RevCommit> commits = git.log().all().call();
+        int count = 0;
+        for (RevCommit commit : commits) {
+            System.out.println("LogCommit: " + commit);
+            count++;
+            System.out.println(count);
+        }
+    }
+
+    public Repository getExistentGitRepository(RepositoryDto repositoryDto) throws IOException {
+        FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
+        repositoryBuilder.setMustExist(true);
+        repositoryBuilder.setGitDir(new File(this.workdir + "/" + repositoryDto.name()));
+        return repositoryBuilder.build();
     }
 }
